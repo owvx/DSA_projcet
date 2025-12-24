375ab70c253a43833536eea0283901876e534c25
Mohamed Ashraf Mohamed <111249107+owvx@users.noreply.github.com>
2025-12-24 20:45:54 +0200

    Update network_analysis.h

--- Network_analysis/network_analysis.h
+++ Network_analysis/network_analysis.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "../generalHeader.h"
-#include "../graphRepresentation/XMLToGraph.h"
+#include "../XMLToGraph/XMLToGraph.h"
 
 
 vector<pair<string,int>> MostInfluencer(map<int,user> users);
@@ -10,4 +10,5 @@ vector<pair<string,int>> MutualUsers(map<int,user>users,vector<string>v);
 vector<pair<string,int>> MostActive(map<int,user>users);
 
 vector<pair<string,int>> SuggestFollowers(map<int,user>users,string idd);
+
 
