4b21cbe8b45641a42eef18169f48aea1dec3c808
Mohamed Ashraf Mohamed <111249107+owvx@users.noreply.github.com>
2025-12-24 03:11:28 +0200

    Update XmlTojson.h

--- XmlToJson/XmlTojson.h
+++ XmlToJson/XmlTojson.h
@@ -10,8 +10,6 @@ void sentenceTag(const string& tagName, const string& sentence,string &jsonStrin
 void normalTag(const string& tagName,string &jsonString,stack<char>&st);
 void justSentence(const string& sentence, string &jsonString);
 
-// New tree-based API
 void convertXMLtoJSONFromTree(XmlNode* root, ofstream &outputFile);
-
-// Backward-compatible wrapper: parse file and call tree-based converter
 void convertXMLtoJSON(ifstream &file, ofstream &outputFile );
+
